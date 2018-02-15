c12f7253842a4e02732458c5c44595dbc3a5cc27
Ian Vlahovic <banddorkfht7@gmail.com>
2018-02-15 20:12:41 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,4 +1,4 @@
-#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
+#Here is the song hash you will be working with. Each key is a song name and each value is the location of its mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
@@ -20,7 +20,7 @@ end
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
 
@@ -33,7 +33,7 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,46 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index { |song, i| puts "#{i + 1}. #{song}" }
+end
+
+def play(song_list)
+  puts "Please enter a song name or number:"
+  song = gets.chomp
+  if (1..9).to_a.include?(song.to_i)
+    puts "Playing #{song_list[song.to_i - 1]}"
+  elsif song_list.include?(song)
+    puts "Playing #{song}"
+  end
+  puts "Invalid input, please try again"
+end
+
+def exit_jukebox
+  puts "Goodbye."
+end
+
+def run(songs)
+  while true
+    puts "Please enter a command:"
+    user_input = gets.chomp
+    if user_input == "exit"
+      exit_jukebox
+      break
+    elsif user_input == "help"
+      help
+    elsif user_input == "list"
+      list(songs)
+    elsif user_input == "play"
+      play(songs)
+    end
+  end
+end
